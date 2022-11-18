65b7901b7c93a4971b55ee1ff03ad44004644fb5
Evgeniy <nikolaev_zheka-zik@bk.ru>
2022-11-18 10:00:37 +0300

    исправляет баг с дублированием таймера

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 const p = document.createElement("p")
 
+let timer;
+
 const createTimerAnimator = () => {
   p.remove
   return (seconds) => {
-    let timer = setInterval(() => {
+    clearInterval(timer)
+    timer = setInterval(() => {
       const second = seconds%60
       const minute = seconds/60%60
       const hour = seconds/60/60%60
@@ -15,9 +18,14 @@ const createTimerAnimator = () => {
         timerEl.append(p)
         p.textContent = "Время вышло"
         p.style.color = "red"
+
+        // удалить, для запуска нового таймера, до завершения запущенного
+        buttonEl.removeAttribute('disabled', "")
+
         clearInterval(timer)
       } else {
-        timerEl.innerHTML = `${(hour >= 10) ? Math.trunc(hour) : `0${Math.trunc(hour)}`}:${(minute >= 10) ? Math.trunc(minute) : `0${Math.trunc(minute)}`}:${(second >= 10) ? second : `0${second}`}`
+        timerEl.innerHTML = 
+          `${(hour >= 10) ? Math.trunc(hour) : `0${Math.trunc(hour)}`}:${(minute >= 10) ? Math.trunc(minute) : `0${Math.trunc(minute)}`}:${(second >= 10) ? second : `0${second}`}`
       }
       --seconds
     }, 1000)
@@ -35,4 +43,7 @@ buttonEl.addEventListener('click', () => {
   let seconds = Number(inputEl.value);
   animateTimer(seconds);
   inputEl.value = '';
+
+  // удалить, для запуска нового таймера, до завершения запущенного
+  buttonEl.setAttribute('disabled', "")
 });
